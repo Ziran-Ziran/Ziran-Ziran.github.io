9a4bbc478db0fbcdfbf241950031a6116615a208
Ziran-Ziran <68945390+Ziran-Ziran@users.noreply.github.com>
2021-02-02 11:25:24 +0800

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: 
+ * @Author: zi ran zi ran
+ * @version: 
+ * @Date: 2020-07-26 16:58:36
+ * @LastEditors: zi ran zi ran
+ * @LastEditTime: 2020-07-27 21:48:31
+ */ 
+document.write("zi ran zi ran");
