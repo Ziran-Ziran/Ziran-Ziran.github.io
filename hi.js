dc87dc3d45764ec28ff3c28811ec00afc1947750
Ziran-Ziran <68945390+Ziran-Ziran@users.noreply.github.com>
2021-02-06 19:39:32 +0800

    Delete hi.js

--- hi.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Descripttion: 
- * @Author: zi ran zi ran
- * @version: 
- * @Date: 2020-07-26 16:58:36
- * @LastEditors: zi ran zi ran
- * @LastEditTime: 2020-07-27 21:48:31
- */ 
-document.write("zi ran zi ran");
